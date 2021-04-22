c0745afe05bb353d19c6e4203f9e24150ea47209
zero <1163201968@qq.com>
2021-04-22 23:40:51 +0800

    HashMap put 源码分析

--- SourceCodeLearning/HashMap 源码分析.java
+++ SourceCodeLearning/HashMap 源码分析.java
@@ -1,3 +1,4 @@
+Java 1.8
 
 //无参构造方法，保存负载因子，DEFAULT_LOAD_FACTOR 为 0.75
 public HashMap() {
@@ -18,18 +19,20 @@ final V putVal(int hash, K key, V value, boolean onlyIfAbsent, boolean evict) {
     //如果当前 Map 还没有元素，则将 Map 的容量初始化为 DEFAULT_INITIAL_CAPACITY（16）
     if ((tab = table) == null || (n = tab.length) == 0)
         n = (tab = resize()).length;//HashMap 为 null 时，resize() 会执行初始化操作。
-    if ((p = tab[i = (n - 1) & hash]) == null)//判断要增加的 key 在 HashMap 中是否存在。
+    if ((p = tab[i = (n - 1) & hash]) == null)//(n - 1) & hash 会将 hash 转成数组的下标。返回数组中该下标对应的 Node ，如果返回 null ，表示插入的是一个新的 key
         tab[i] = newNode(hash, key, value, null);//如果是一个新的 key，则创建一个新的 Node ，存放到数组中
-    else {
+    else {//如果数组中在 key 对应的下标处已经存在一个节点
         Node<K,V> e; K k;
+        //如果数组中原始的那个节点的 key 以及 hash 与当前要插入的节点相同，说明是同一个 key，将 e 指向旧的节点
         if (p.hash == hash && ((k = p.key) == key || (key != null && key.equals(k))))
             e = p;
-        else if (p instanceof TreeNode)
+        else if (p instanceof TreeNode) //如果数组中原始节点是一个红黑树的节点，则执行插入红黑树节点的逻辑
             e = ((TreeNode<K,V>)p).putTreeVal(this, tab, hash, key, value);
-        else {
+        else {//如果数组中的原始节点就是一个链表节点，且原始节点的 key 和要插入的 key 不一样，说明发生了哈希碰撞
             for (int binCount = 0; ; ++binCount) {
-                if ((e = p.next) == null) {
-                    p.next = newNode(hash, key, value, null);
+                if ((e = p.next) == null) {//遍历到链表的最后一个节点
+                    p.next = newNode(hash, key, value, null);//将新的节点插入到链表的末尾
+                    //TREEIFY_THRESHOLD 的值为 8，链表的长度超过了 8 个，会执行链表转红黑树的操作。
                     if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
                         treeifyBin(tab, hash);
                     break;
